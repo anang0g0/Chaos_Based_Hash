b94acd9d437904690633a2dde18aa1bf264aafb9
Osamu Sakai <fumumue@protonmail.com>
2021-05-18 03:33:57 +0900

    modified

--- rho.py
+++ rho.py
@@ -10,7 +10,7 @@ def gcd(a, b):
 def f(x):
     return x*x+1
 
-N = 652177 #2**(2**6)+1 
+N = 761 # 652177 #2**(2**6)+1 
 print(N)
 for j in range(N):
     count=0
@@ -20,21 +20,24 @@ for j in range(N):
     for i in range(N):  #range(10**7):
         #x = f(i) % N
         #y = f(f(i)) % N
-        #z = f(f(f(j))) % N
+        z = f(f(f(z))) % N
         x = f(x) % N
         y = f(f(y)) % N
         if x!=y:
             g = gcd(x-y, N)
+        if x==j:
+            print("same",x,i,g)
 
-        if x == j: #  and j != x and x != y:
+        #if z < j and j < x and x < y:
             #if i==f(i):
-            print(j,x,i,g)
+        #    print(j,x,y,z,i,g)
             #count=count+1
-            #if 1<g<N:
-            #    print(g,j)
+        #if 1<g<N:
+        #    print(g,j)
+        
             break
-            #exit(1)
-            
+            if x==j:
+                print ("precious")
     #if count > 0:
     #    print(count,j)
 
